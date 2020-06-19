082a7af22f8057e88c949800aab75f34b47cf6a1
vagrant <vagrant@homestead>
2020-06-19 09:26:54 +0000

    途中セーブ

---
+++ front/src/store/profile/reducer.ts
@@ -0,0 +1,26 @@
+import { reducerWithInitialState } from "typescript-fsa-reducers";
+import { Profile } from "../../domain/entity/profile";
+import profileActions from "./actions";
+
+const init: Profile = {
+  name: "",
+  description: "",
+  birthday: "",
+  gender: "",
+  address: {
+    postalcode: "",
+    prefecture: "",
+    city: "",
+    restAddress: ""
+  }
+};
+
+const profileReducer = reducerWithInitialState(init)
+  .case(profileActions.setProfile, (state, payload) => ({
+    ...state, ...payload
+  }))
+  .case(profileActions.setAddress, (state, payload) => ({
+    ...state, address: { ...state.address, ...payload }
+  }));
+
+export default profileReducer;
